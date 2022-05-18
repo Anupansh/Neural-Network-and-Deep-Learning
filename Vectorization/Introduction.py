f34200331d01cbd7bf216bbf9e43d48251c053b0
Anupansh <anupanshaggarwal@gmail.com>
2022-05-18 23:45:33 +0530

    Cat Recognizer with notes

--- Vectorization/Introduction.py
+++ Vectorization/Introduction.py
@@ -1,4 +1,41 @@
 # Vectorization is a process of lessening the number of for loops in
-
-import numpy as np
+import math
 import time
+import numpy as np
+
+a = np.random.rand(1000000)
+b = np.random.rand(1000000)
+
+# Without vectorization
+c = 0
+tic = time.time()
+for i in range(100000):
+    c += a[i] * b[i]
+toc = time.time()
+print("Time taken to run for loop", (toc - tic) * 1000, "ms with value", c)
+
+# With vectorization
+tic = time.time()
+c = np.dot(a, b)
+toe = time.time()
+print("Time taken to run vectorized version", (toe - tic) * 1000, "ms with value ", c)
+
+# Another example
+
+
+n = 1000000
+v = np.random.random(n)
+u = np.zeros((n, 1))
+
+# Without Vectorization
+tic = time.time()
+for i in range(n):
+    u[i] = math.exp(v[i])
+toc = time.time()
+print("Time taken to run for loop", (toc - tic) * 1000, "ms")
+
+# Without Vectorization
+tic = time.time()
+u = np.exp(v)
+toc = time.time()
+print("Time taken to run for loop", (toc - tic) * 1000, "ms")
---
+++ Vectorization/Test.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = [[1, 2], [3, 4], [5, 6], [7, 8]]
+b = [[5, 6], [7, 8]]
+print("A", a)
+print("B", b)
+print(np.dot(a, b))
+
+a = [[1, 2]]
+b = [[3], [2]]
+
+print(np.dot(b, a))
+print(np.dot(a, b))
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+arr = arr.reshape(4,-1)
+print(arr)
