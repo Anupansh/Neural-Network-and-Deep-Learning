1eb63365d97018e8aedd3f3a979b6ea18eb2947e
Anupansh <anupanshaggarwal@gmail.com>
2022-05-22 00:09:58 +0530

    Week 3 Assignment completed

--- Vectorization/Test.py
+++ Vectorization/Test.py
@@ -1,3 +1,5 @@
+import copy
+
 import numpy as np
 
 a = [[1, 2], [3, 4], [5, 6], [7, 8]]
@@ -13,5 +15,19 @@ print(np.dot(b, a))
 print(np.dot(a, b))
 
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-arr = arr.reshape(4,-1)
+arr = arr.reshape(4, -1)
 print(arr)
+
+
+def checkDeepCopy(W):
+    W1 = copy.deepcopy(W)
+    W1 += 1
+    print("Inside",W1)
+
+W = checkDeepCopy(4)
+print("Outside",W)
+
+x = np.random.rand(3, 2)
+
+y = np.sum(x, axis=0, keepdims=True)
+print(y.shape)
